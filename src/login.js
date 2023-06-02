2fe33976142b5205d1a7372e2b201f21d053b80b
Superior125 <90008321+Superior126@users.noreply.github.com>
2023-06-01 22:04:13 -0400

    updates

--- src/login.js
+++ src/login.js
@@ -1,15 +1,21 @@
 import Logo from "./assets/Lif_Logo.png"
+import './login.css';
 
 function Login() {
     return(
         <div className="LoginPage">
             <div className="LoginForm">
                 <img src={Logo} alt="Lif-Logo" />
+                <h1>Login With Lif</h1>
                 <form>
-                    <input type="Username" id="Username" />
-                    <input type="Password" id="Password" />
-                    <button type="buttton">Login</button>
+                    <input type="Username" id="Username" placeholder="Username" />
+                    <br />
+                    <input type="Password" id="Password" placeholder="Password"/>
+                    <br />
+                    <button type="button">Login</button>
                 </form>
+                <br />
+                <a href="#">Forgot Password</a>
             </div>
         </div>
     )
