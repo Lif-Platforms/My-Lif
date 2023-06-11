29ade322392301e4f9147ab9265843f6d2c55450
Superior125 <90008321+Superior126@users.noreply.github.com>
2023-06-11 12:57:21 -0400

    updates

--- src/login.js
+++ src/login.js
@@ -1,7 +1,29 @@
 import Logo from "./assets/Lif_Logo.png"
 import './login.css';
+import { login } from "./scripts/login";
 
 function Login() {
+
+    // Function for handling the login process
+    async function handle_login() {
+        // Changes the login buttons state
+        const login_button = document.getElementById("login_button");
+        login_button.innerHTML = "Logging In...";
+        login_button.disabled = true;
+
+        // Gets the username and password from the login form
+        const username_input = document.getElementById('Username');
+        const password_input = document.getElementById('Password');
+
+        const username = username_input.value;
+        const password = password_input.value; 
+
+        // Logs in the user with the lif auth server
+        const login_status = await login(username, password);
+
+        
+    }   
+
     return(
         <div className="LoginPage">
             <div className="LoginForm">
@@ -12,7 +34,7 @@ function Login() {
                     <br />
                     <input type="Password" id="Password" placeholder="Password"/>
                     <br />
-                    <button type="button">Login</button>
+                    <button type="button" id='login_button' onClick={handle_login}>Login</button>
                 </form>
                 <br />
                 <a href="#">Forgot Password</a>
