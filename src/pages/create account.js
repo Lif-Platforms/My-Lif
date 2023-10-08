fbe1c33e121ef2184c09ea9e761defc410f86f32
Superior125 <90008321+Superior126@users.noreply.github.com>
2023-10-07 22:53:43 -0400

    Updates to account creation

--- src/pages/create account.js
+++ src/pages/create account.js
@@ -1,9 +1,95 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "../css/create account.css";
 import LifLogo from "../assets/Lif_Logo.png";
 
 function CreateAccount() {
     const [accountState, setAccountState] = useState('welcome');
+    const [unsavedChanges, setUnsavedChanges] = useState(false);
+    const unsavedChangesRef = useRef(unsavedChanges);
+    const [accountData, setAccountData] = useState({username: null, email: null, password: null});
+
+    useEffect(() => {
+        // Update the ref whenever unsavedChanges changes
+        unsavedChangesRef.current = unsavedChanges;
+      }, [unsavedChanges]);
+
+    useEffect(() => {
+        console.log("Adding beforeunload event listener");
+        window.addEventListener('beforeunload', confirmExit);
+      
+        return () => {
+          console.log("Removing beforeunload event listener");
+          window.removeEventListener('beforeunload', confirmExit);
+        };
+      }, []);
+
+    // Function to display the confirmation message
+    const confirmExit = (e) => {
+        console.log(unsavedChangesRef.current);
+        if (unsavedChangesRef.current) {
+            e.preventDefault();
+            e.returnValue = 'Your Changes Will Not Be Saved!'; // Required for some browsers
+        }
+    };
+
+    useEffect(() => {
+        console.log(accountData);
+    }, [accountData]);
+
+    function handle_navigate(info_type, element_id, nav_page) {
+        // Get username
+        const info = document.getElementById(element_id).value; 
+
+        // Update button
+        document.getElementById("submit-button").innerHTML = "Loading...";
+
+        // Check if username is already in use
+        fetch(`${process.env.REACT_APP_AUTH_URL}/check_account_info_usage/${info_type}/${info}`)
+            .then(response => {
+                if (response.ok) {
+                return response.json(); // Convert response to JSON
+                } else {
+                    return response.json().then(json => { throw json; });
+                }
+            })
+            .then(data => {
+                // Work with the data
+                console.log(data);
+
+                console.log(info_type);
+                
+                // Navigate to next page if status was ok
+                if (data.Status === "Ok") {
+                    document.getElementById("submit-button").innerHTML = "Next";
+                    document.getElementById(element_id).value = ""; // Reset the value of the user input
+                    document.getElementById('account-status').innerHTML = "";
+
+                    // Create a clone of the 'accountData' useState and update its info
+                    const updatedAccountData = { ...accountData };
+                    updatedAccountData[info_type] = info;
+
+                    console.log(updatedAccountData);
+
+                    // Save account info for later
+                    setAccountData(updatedAccountData);
+
+                    setAccountState(nav_page);
+                }
+            })
+            .catch(error => {
+                // Handle any errors
+                console.error(error);
+
+                // Update account status for user
+                document.getElementById('account-status').innerHTML = error.detail;
+                document.getElementById("submit-button").innerHTML = "Next";
+        });
+    }
+
+    function handle_welcome_navigate() {
+        setUnsavedChanges(true);
+        setAccountState("select_username");
+    }
 
     if (accountState === "welcome") {
         return ( 
@@ -13,7 +99,7 @@ function CreateAccount() {
                         <img src={LifLogo} alt="Lif Logo" />
                         <h1>Welcome To Lif!</h1>
                         <p>Your Social Starts Here</p>
-                        <button onClick={() => setAccountState('select_username')}>Lets Go!</button>
+                        <button onClick={() => handle_welcome_navigate()}>Lets Go!</button>
                     </div>
                 </div>
             </div>
@@ -27,7 +113,8 @@ function CreateAccount() {
                         <p>Your username is one of the most important parts of your Lif Account. Its the name that others will see when they look at your profile. Choose wisely, they cannot be changed later.</p>
                         <input id="username" placeholder="Username" />
                         <br />
-                        <button onClick={() => setAccountState('add_email')}>Next</button>
+                        <button id="submit-button" onClick={() => handle_navigate("username", "username", "add_email")}>Next</button>
+                        <span id="account-status" className="account-status" />
                     </div>
                 </div>
             </div>
@@ -41,7 +128,8 @@ function CreateAccount() {
                         <p>Your email is crucial to your Lif account. It allows us to send you import emails, such as account recovery emails.</p>
                         <input id="email" placeholder="Email" />
                         <br />
-                        <button onClick={() => setAccountState('create_password')}>Next</button>
+                        <button id="submit-button" onClick={() => handle_navigate("email", "email", "add_email")}>Next</button>
+                        <span id="account-status" className="account-status" />
                     </div>
                 </div>
             </div>
