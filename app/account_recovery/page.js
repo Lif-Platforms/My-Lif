b9ab186e9bdc8869187aacfc041e803d55b5a779
Superior125 <90008321+Superior126@users.noreply.github.com>
2024-10-04 01:19:17 -0400

    Migrate account recovery page

---
+++ app/account_recovery/page.js
@@ -0,0 +1,10 @@
+import Panel from "@/components/account_recovery/panel/panel";
+import styles from './page.module.css';
+
+export default function AccountRecovery() {
+    return (
+        <div className={styles.page}>
+            <Panel />
+        </div>
+    );
+}
